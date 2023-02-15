82be87623b27cd33a65cdc4842da693059959b43
Archerprop <archerprop@outlook.com>
2023-02-15 18:47:28 -0500

    first commit

---
+++ src/main/java/com/archerprop/peja/Peja.java
@@ -0,0 +1,21 @@
+package com.archerprop.peja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aschente
+ */
+@SpringBootApplication
+public class Peja {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Peja.class, args);
+    }
+}
