8ad0186e1c28d88ed95aa10fb59c84bac6e59d95
Cristian Marin <cmarinb@ucentral.edu.co>
2023-05-17 14:09:12 -0500

    alert's campos todos formularios

--- src/main/java/com/archerprop/peja/service/Impl/UsuarioServiceImpl.java
+++ src/main/java/com/archerprop/peja/service/Impl/UsuarioServiceImpl.java
@@ -77,7 +77,13 @@ public class UsuarioServiceImpl implements UsuarioService {
                 new BCryptPasswordEncoder().encode(registroDTO.getClave()),
                 roles);
         return usuarioRepositorio.save(usuario);
-    }
+    }// El método guardar es una función que guarda un nuevo usuario en una base de
+     // datos. El método acepta un objeto UsuarioRegistroDTO que contiene los datos
+     // del usuario y una cadena de texto userRol que representa el rol del
+     // usuario.En otras palabras, este método acepta datos de registro de usuario,
+     // comprueba si ya existe un usuario con el correo electrónico proporcionado,
+     // crea un nuevo objeto Usuario con los datos proporcionados, guarda el objeto
+     // Usuario en la base de datos y devuelve el objeto Usuario guardado.
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -86,16 +92,29 @@ public class UsuarioServiceImpl implements UsuarioService {
             throw new UsernameNotFoundException("Usuario no encontrado");
         }
         return new User(usuario.getCorreo(), usuario.getClave(), getAuthorities(usuario.getRoles()));
-    }
+    }// Este método se utiliza para cargar los detalles de un usuario a través de su
+     // correo electrónico. El método busca un objeto Usuario en la base de datos con
+     // el correo electrónico especificado, crea un objeto UserDetails con los
+     // detalles del usuario encontrado y devuelve este objeto UserDetails. Si no se
+     // encuentra un usuario con el correo electrónico especificado, el método arroja
+     // una excepción.
 
     private Collection<? extends GrantedAuthority> getAuthorities(Collection<Rol> roles) {
         return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
-    }
+    }// Este método se utiliza para crear una lista de autoridades (GrantedAuthority)
+     // a partir de una colección de roles (Rol). El método itera a través de la
+     // colección de roles, crea un objeto SimpleGrantedAuthority para cada rol y los
+     // agrega a una lista de autoridades. El método devuelve la lista de autoridades
+     // creadas.
 
     @Override
     public Usuario buscarUsuario(String correo) {
         return usuarioRepositorio.findByCorreo(correo);
-    }
+    }// Este método se utiliza para buscar un objeto Usuario en la base de datos a
+     // través de su correo electrónico. El método busca un objeto Usuario en la base
+     // de datos con el correo electrónico especificado, y devuelve el objeto Usuario
+     // encontrado si existe. Si no se encuentra un usuario con el correo electrónico
+     // especificado, el método devuelve nulo.
 
     @Override
     public List<Usuario> listarUsuariosAdmin() {
@@ -104,7 +123,10 @@ public class UsuarioServiceImpl implements UsuarioService {
                 .filter(usuario -> usuario.getRoles().stream().anyMatch(rol -> rol.getName().equals("ADMIN")))
                 .collect(Collectors.toList());
         return usuariosAdmin;
-    }
+    }// Este método se utiliza para obtener una lista de todos los usuarios que
+     // tienen el rol de "ADMIN". El método busca todos los usuarios en la base de
+     // datos y luego filtra solo aquellos que tienen el rol de "ADMIN". La lista
+     // filtrada de usuarios con el rol de "ADMIN" se devuelve como resultado.
 
     @Override
     public List<Usuario> listarUsuariosDocente() {
@@ -113,7 +135,10 @@ public class UsuarioServiceImpl implements UsuarioService {
                 .filter(usuario -> usuario.getRoles().stream().anyMatch(rol -> rol.getName().equals("DOCENTE")))
                 .collect(Collectors.toList());
         return usuariosDocente;
-    }
+    }// Este método se utiliza para obtener una lista de todos los usuarios que
+     // tienen el rol de "DOCENTE". El método busca todos los usuarios en la base de
+     // datos y luego filtra solo aquellos que tienen el rol de "DOCENTE". La lista
+     // filtrada de usuarios con el rol de "DOCENTE" se devuelve como resultado.
 
     @Override
     public List<Usuario> listarUsuariosEstudiante() {
@@ -122,7 +147,11 @@ public class UsuarioServiceImpl implements UsuarioService {
                 .filter(usuario -> usuario.getRoles().stream().anyMatch(rol -> rol.getName().equals("ESTUDIANTE")))
                 .collect(Collectors.toList());
         return usuariosEstudiante;
-    }
+    }// Este método se utiliza para obtener una lista de todos los usuarios que
+     // tienen el rol de "ESTUDIANTE". El método busca todos los usuarios en la base
+     // de datos y luego filtra solo aquellos que tienen el rol de "ESTUDIANTE". La
+     // lista filtrada de usuarios con el rol de "ESTUDIANTE" se devuelve como
+     // resultado.
 
     @Override
     @Transactional
@@ -139,7 +168,13 @@ public class UsuarioServiceImpl implements UsuarioService {
             System.out.println("Error al eliminar el usuario: " + e.getMessage());
             return false;
         }
-    }
+    }// este método se utiliza para eliminar un objeto Usuario de la base de datos.
+     // El método utiliza dos consultas SQL para eliminar el objeto Usuario y sus
+     // relaciones de roles de la base de datos. Si se completa con éxito la
+     // eliminación, el método devuelve true. Si ocurre un error, se imprime un
+     // mensaje de error y el método devuelve false. El método utiliza una
+     // transacción para asegurar que todas las operaciones de eliminación se
+     // completen con éxito o se reviertan en caso de que ocurra un error.
 
     @Override
     public boolean modificar(UsuarioRegistroDTO registroDTO, String userRol) {
@@ -161,6 +196,13 @@ public class UsuarioServiceImpl implements UsuarioService {
             System.out.println("Error al modificar el usuario: " + e.getMessage());
             return false;
         }
-    }
+    }// Este método recibe un objeto UsuarioRegistroDTO y un String que indica el rol
+     // del usuario. Busca en el repositorio de usuarios un usuario con el correo
+     // electrónico proporcionado en el objeto UsuarioRegistroDTO y crea un nuevo
+     // objeto Usuario con los datos actualizados del objeto UsuarioRegistroDTO y el
+     // rol proporcionado. Luego guarda este nuevo objeto Usuario en el repositorio
+     // de usuarios y devuelve verdadero si la operación fue exitosa y falso si
+     // ocurrió algún error. Si ocurre algún error, imprime un mensaje de error
+     // en la consola.
 
 }
--- src/main/java/com/archerprop/peja/service/UsuarioService.java
+++ src/main/java/com/archerprop/peja/service/UsuarioService.java
@@ -23,4 +23,24 @@ public interface UsuarioService extends UserDetailsService {
 
     public boolean modificar(UsuarioRegistroDTO usuarioDTO, String rol);
 
-}
+}// La interfaz extiende la interfaz UserDetailsService, lo que significa que
+ // cualquier clase que implemente UsuarioService debe proporcionar una
+ // implementación para el método loadUserByUsername que recibe como parámetro el
+ // nombre de usuario y devuelve detalles de usuario.
+
+// Los métodos definidos en la interfaz UsuarioService son:
+
+// guardar: Recibe un objeto UsuarioRegistroDTO y un nombre de rol como
+// parámetros y devuelve un objeto Usuario después de guardarlo en la base de
+// datos.buscarUsuario: Recibe un correo electrónico como parámetro y devuelve
+// un objeto Usuario que tiene ese correo electrónico.
+// listarUsuariosAdmin: Devuelve una lista de objetos Usuario que tienen el rol
+// de administrador.
+// listarUsuariosDocente: Devuelve una lista de objetos Usuario que tienen el
+// rol de docente.
+// istarUsuariosEstudiante: Devuelve una lista de objetos Usuario que tienen el
+// rol de estudiante.
+// eliminar: Recibe un objeto Usuario como parámetro y elimina ese usuario de la
+// base de datos.
+// modificar: Recibe un objeto UsuarioRegistroDTO y un nombre de rol como
+// parámetros y modifica el usuario correspondiente en la base de datos.
